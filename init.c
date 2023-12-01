354a142e677ea3171852710d3233a1e3239f331f
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-02 04:08:33 +0900

    Update init.c

--- init.c
+++ init.c
@@ -1,65 +1,19 @@
 #include "init.h"
 
-void InitSDL(void) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("[ERROR] in InitSDL(): %s", SDL_GetError());
-        exit(1);
-    }
 
-    app.window = SDL_CreateWindow("Dodge!", SDL_WINDOWPOS_UNDEFINED,
-                                  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                  SCREEN_HEIGHT, 0);
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-    app.renderer = SDL_CreateRenderer(app.window, -1, SDL_RENDERER_ACCELERATED);
-    IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
 
-    return;
-}
 
-void InitTTF(void) {
-    if (TTF_Init() < 0) {
-        printf("[ERROR] in InitTTF(): %s", SDL_GetError());
-        exit(1);
-    }
-
-    app.font = TTF_OpenFont("./ttf/LiberationSans-Regular.ttf", 20);
-
-    return;
-}
-
-void QuitSDL(int flag) {
-    Mix_FreeChunk(death_effect);
-    Mix_FreeMusic(bgm);
-    SDL_DestroyTexture(player[i].texture);
-    SDL_FreeSurface(score_board.surface);
-    SDL_DestroyTexture(game_over.texture);
-    SDL_DestroyTexture(score_board.texture);
-    
-    SDL_DestroyRenderer(app.renderer);
-    SDL_DestroyWindow(app.window);
-    SDL_CloseAudio();
-
-    QuitTTF();
-    IMG_Quit();
-    SDL_Quit();
-    exit(flag);
-
-    return;
-}
-
-void QuitTTF(void) {
-    TTF_CloseFont(app.font);
-    TTF_Quit();
-
-    return;
-}
 
 void InitMemorySet(void) {
     memset(&app, 0, sizeof(App));
     memset(&game_over, 0, sizeof(Entity));
-    memset(&player, 0, sizeof(Entity));
+    memset(&player, 0, BUFSIZE*sizeof(Entity));
+    memset(&BALL, 0, sizeof(Entity));
+    memset(&field, 0, sizeof(Entity));
+    memset(&LGoalnet, 0, sizeof(Entity));
+    memset(&RGoalnet, 0, sizeof(Entity));
     memset(&score_board, 0, sizeof(Text));
-
+    memset(&BALL, 0, 2*sizeof(Item));   
     return;
 }
 
@@ -78,47 +32,47 @@ void InitScoreBoard(void) {
 }
 
 void InitPlayer1(void) {
-    player.texture = IMG_LoadTexture(app.renderer, "./gfx/12.png");
-    player.pos.x = SCREEN_WIDTH / 2;
-    player.pos.y = SCREEN_HEIGHT / 2;
-    SDL_QueryTexture(player.texture, NULL, NULL, &(player.pos.w),
-                     &(player.pos.h));
+    player[0].texture = IMG_LoadTexture(app.renderer, "./gfx/12_1.png");
+    player[0].pos.x = SCREEN_WIDTH / 2;
+    player[0].pos.y = SCREEN_HEIGHT / 2;
+    SDL_QueryTexture(player[0].texture, NULL, NULL, &(player[0].pos.w),
+                     &(player[0].pos.h));
 
     return;
 }
 void InitPlayer2(void) {
-    player.texture = IMG_LoadTexture(app.renderer, "./gfx/14.png");
-    player.pos.x = SCREEN_WIDTH / 2;
-    player.pos.y = SCREEN_HEIGHT / 2;
-    SDL_QueryTexture(player.texture, NULL, NULL, &(player.pos.w),
-                     &(player.pos.h));
+    player[1].texture = IMG_LoadTexture(app.renderer, "./gfx/14_1.png");
+    player[1].pos.x = SCREEN_WIDTH / 2;
+    player[1].pos.y = SCREEN_HEIGHT / 2;
+    SDL_QueryTexture(player[1].texture, NULL, NULL, &(player[1].pos.w),
+                     &(player[1].pos.h));
 
     return;
 }
 void InitPlayer3(void) {
-    player.texture = IMG_LoadTexture(app.renderer, "./gfx/13.png");
-    player.pos.x = SCREEN_WIDTH / 2;
-    player.pos.y = SCREEN_HEIGHT / 2;
-    SDL_QueryTexture(player.texture, NULL, NULL, &(player.pos.w),
-                     &(player.pos.h));
+    player[2].texture = IMG_LoadTexture(app.renderer, "./gfx/13_1.png");
+    player[2].pos.x = SCREEN_WIDTH / 2;
+    player[2].pos.y = SCREEN_HEIGHT / 2;
+    SDL_QueryTexture(player[2].texture, NULL, NULL, &(player[2].pos.w),
+                     &(player[2].pos.h));
 
     return;
 }
 void InitLgoalnet(void) {
-    player.texture = IMG_LoadTexture(app.renderer, "./gfx/21.png");
+    LGoalnet.texture = IMG_LoadTexture(app.renderer, "./gfx/21.png");
     LGoalnet.pos.x=5;
     LGoalnet.pos.y=480;
-    SDL_QueryTexture(player.texture, NULL, NULL, &(player.pos.w),
-                     &(player.pos.h));
+    SDL_QueryTexture(LGoalnet.texture, NULL, NULL, &(LGoalnet.pos.w),
+                     &(LGoalnet.pos.h));
 
     return;
 }
 void InitRgoalnet(void) {
-    player.texture = IMG_LoadTexture(app.renderer, "./gfx/20.png");
+    RGoalnet.texture = IMG_LoadTexture(app.renderer, "./gfx/20.png");
     RGoalnet.pos.x=595;
     RGoalnet.pos.y=480;
-    SDL_QueryTexture(player.texture, NULL, NULL, &(player.pos.w),
-                     &(player.pos.h));
+    SDL_QueryTexture(RGoalnet.texture, NULL, NULL, &(RGoalnet.pos.w),
+                     &(RGoalnet.pos.h));
 
     return;
 }
@@ -128,4 +82,11 @@ void InitBall(void){
                      &(game_over.pos.h));
     BALL.pos.x = 320;
     BALL.pos.y = 70;
+}
+void InitGameOver(void) {
+    game_over.texture = IMG_LoadTexture(app.renderer, "./gfx/pic30.png");
+    SDL_QueryTexture(game_over.texture, NULL, NULL, &(game_over.pos.w),
+                     &(game_over.pos.h));
+    game_over.pos.x = 0;
+    game_over.pos.y = 0;
 }
