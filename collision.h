6e7f3929e26ad613dbcd69cf7da41c9de0f91a93
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-03 00:21:03 +0900

    Update collision.h

--- collision.h
+++ collision.h
@@ -4,11 +4,15 @@
 #include "init.h"
 #include "defs.h"
 #include "utils.h"
+#include "gravity.h"
 #include "main.h"
-
+#include "action.h"
+#include "item.h"
+#include "main.h"
+#include "utils.h"
+#include "player.h"
 
 void Act(void);
-
 void elasticCollision2D(Entity *object1, Entity *object2);
 
 
