774c6775e4895d95c4ee2cc124c79b60ab030ec9
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-03 00:25:10 +0900

    Update init.h

--- init.h
+++ init.h
@@ -5,12 +5,17 @@
 #include "utils.h"
 void InitMemorySet(void);
 void InitScoreBoard(void);
-void InitPlayer1();
+void Initfield(void);
+void InitPlayer1(void);
+void InitPlayer1_1(void);
 void InitPlayer2(void);
+void InitPlayer2_1(void);
 void InitPlayer3(void);
+void InitPlayer3_1(void);
 void InitLgoalnet(void);
 void InitRgoalnet(void);
 void InitBall(void);
 void InitGameOver(void);
+void initial_value(void);
 
 #endif
